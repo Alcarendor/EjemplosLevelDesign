b23f4ee93f3e24b97234d95c026fbf6ac5d1479d
Roldan <roldangp@hotmail.com>
2021-10-28 16:08:27 +0200

    Inicio activador animaciones

---
+++ Assets/Scripts/Prototype_Character/Activator_animation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator_animation : MonoBehaviour, Interface_Activation
+{
+    public AnimationClip[] animationList;
+    // Start is called before the first frame update
+    public void ExecuteAction()
+    {
+
+    }
+}
