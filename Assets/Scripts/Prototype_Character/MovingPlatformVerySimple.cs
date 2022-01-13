f057e5a6cef43c00b36c9877114aa7e84e17192b
Alcarendor <roldangp@gmail.com>
2022-01-13 13:58:11 +0100

    Finalizada plataforma movil
    A falta de testear, script de plataforma móvil acabado.

--- Assets/Scripts/Prototype_Character/MovingPlatformVerySimple.cs
+++ Assets/Scripts/Prototype_Character/MovingPlatformVerySimple.cs
@@ -20,13 +20,39 @@ public class MovingPlatformVerySimple : MonoBehaviour
     [Tooltip("Si solo se pone un elemento, se aplicará el mismo tiempo a todos los movimientos del array. Si se pone más de un tiempo pero el valor es diferente al tamaño del listado de Waypoints, dará error.")]
     [SerializeField] private List<float> listaTiempos;
 
+    [Header("Loops")]
+    [Tooltip("Ponga -1 para un número ilimitado de loops.")]
+    [SerializeField] private int numeroLoops = -1;
+    [Tooltip("El tipo de loop 'YoYo' indica que hará la misma secuencia a la inversa.")]
+    [SerializeField] private LoopType tipoLoop = LoopType.Yoyo;
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; 0 < Waypoints.Count; i++)
+        Sequence platformSequence = DOTween.Sequence();
+        platformSequence.SetUpdate(tipoUpdate);
+        platformSequence.SetEase(tipoMovimiento);
+        platformSequence.SetLoops(numeroLoops, tipoLoop);
+        Debug.Log("Lista de tiempos " + listaTiempos.Count);
+        for (int i = 0; i < Waypoints.Count; i++)
         {
+            Debug.Log(i);
+            // Esta ñapa del tiempo no me convence.
+            float tiempoActual = 0;
+            if (listaTiempos.Count == 1)
+                tiempoActual = listaTiempos[0];
+            else
+                tiempoActual = listaTiempos[i];
+
+            platformSequence.Append(transform.DOMove(Waypoints[i].position, tiempoActual));
+            if (tiempoPausa > 0)
+            {
+                platformSequence.AppendInterval(tiempoPausa);
+                //platformSequence.Append(transform.DOMove(Waypoints[i].position, tiempoPausa));
+            }
 
         }
+
         //Tween myTween = transform.DOMove(Waypoint.position, 10);
         //myTween.SetLoops(-1, LoopType.Yoyo);
         //myTween.SetUpdate(UpdateType.Fixed);
